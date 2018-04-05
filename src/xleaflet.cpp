0abe67ea0492216460f5132d75f60888efc1b22f
martinRenou <martin.renou@gmail.com>
2018-04-05 16:39:51 +0200

    Created ximage_overlay class

--- src/xleaflet.cpp
+++ src/xleaflet.cpp
@@ -4,6 +4,7 @@
 #include "xleaflet/xtile_layer.hpp"
 #include "xleaflet/xui_layer.hpp"
 #include "xleaflet/xmarker.hpp"
+#include "xleaflet/ximage_overlay.hpp"
 
 template class XLEAFLET_API xw::xmaterialize<xleaflet::xmap>;
 template xw::xmaterialize<xleaflet::xmap>::xmaterialize();
