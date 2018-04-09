ee15d69a68d0c0e9b264186b5026f9b7ebbbe5fd
martinRenou <martin.renou@gmail.com>
2018-04-09 19:46:09 +0200

    Implement add_layer, remove_layer and clear_layers for xlayer_group

--- include/xleaflet/xlayer_group.hpp
+++ include/xleaflet/xlayer_group.hpp
@@ -9,6 +9,7 @@
 #ifndef XLEAFLET_LAYER_GROUP_HPP
 #define XLEAFLET_LAYER_GROUP_HPP
 
+#include <algorithm>
 #include <vector>
 
 #include "xwidgets/xmaterialize.hpp"
@@ -43,6 +44,17 @@ namespace xleaflet
 
         xeus::xjson get_state() const;
         void apply_patch(const xeus::xjson&);
+
+        template <class T>
+        void add_layer(const xlayer<T>& l);
+
+        template <class T>
+        void add_layer(xlayer<T>&& l);
+
+        template <class T>
+        void remove_layer(const xlayer<T>& l);
+
+        void clear_layers();
 
         XPROPERTY(layer_list_type, derived_type, layers);
 
@@ -80,6 +92,68 @@ namespace xleaflet
         base_type::apply_patch(patch);
 
         XOBJECT_SET_PROPERTY_FROM_PATCH(layers, patch);
+    }
+
+    template <class D>
+    template <class T>
+    inline void xlayer_group<D>::add_layer(const xlayer<T>& l)
+    {
+#ifdef _MSC_VER
+        this->layers().emplace_back(xw::make_id_holder<layer_type>(l.id()));
+#else
+        this->layers().emplace_back(xw::make_id_holder<xlayer>(l.id()));
+#endif
+        xeus::xjson state;
+        XOBJECT_SET_PATCH_FROM_PROPERTY(layers, state);
+        this->send_patch(std::move(state));
+    }
+
+    template <class D>
+    template <class T>
+    inline void xlayer_group<D>::add_layer(xlayer<T>&& l)
+    {
+        this->layers().emplace_back(xw::make_owning_holder(std::move(l)));
+        xeus::xjson state;
+        XOBJECT_SET_PATCH_FROM_PROPERTY(layers, state);
+        this->send_patch(std::move(state));
+    }
+
+    template <class D>
+    template <class T>
+    inline void xlayer_group<D>::remove_layer(const xlayer<T>& l)
+    {
+#ifdef _MSC_VER
+        template <class T>
+        using layer_type = xlayer<T>;
+
+        this->layers().erase(
+            std::remove_if(
+                this->layers().begin(), this->layers().end(),
+                [&l](xw::xholder<layer_type> _l){return _l.id() == l.id();}
+            ),
+            this->layers().end()
+        );
+#else
+        this->layers().erase(
+            std::remove_if(
+                this->layers().begin(), this->layers().end(),
+                [&l](xw::xholder<xlayer> _l){return _l.id() == l.id();}
+            ),
+            this->layers().end()
+        );
+#endif
+        xeus::xjson state;
+        XOBJECT_SET_PATCH_FROM_PROPERTY(layers, state);
+        this->send_patch(std::move(state));
+    }
+
+    template <class D>
+    inline void xlayer_group<D>::clear_layers()
+    {
+        this->layers() = {};
+        xeus::xjson state;
+        XOBJECT_SET_PATCH_FROM_PROPERTY(layers, state);
+        this->send_patch(std::move(state));
     }
 
     template <class D>
