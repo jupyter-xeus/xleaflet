505a8cbf2f2ba508559f5ef600ecbf181d8ef8cc
Sylvain Corlay <sylvain.corlay@gmail.com>
2020-09-22 16:24:06 +0200

    Release 0.11.2

--- include/xleaflet/xleaflet_config.hpp
+++ include/xleaflet/xleaflet_config.hpp
@@ -25,7 +25,7 @@
 // Project version
 #define XLEAFLET_VERSION_MAJOR 0
 #define XLEAFLET_VERSION_MINOR 11
-#define XLEAFLET_VERSION_PATCH 1
+#define XLEAFLET_VERSION_PATCH 2
 
 // Binary version
 #define XLEAFLET_BINARY_CURRENT 1
